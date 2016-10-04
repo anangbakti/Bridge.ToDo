2a9f6f597cd36aba040aaa561068ce73b9d75523
anang.bakti <anang.bakti@ptdmc.co.id>
2016-10-05 04:47:32 +0700

    Add output folder

---
+++ Bridge/output/bridge.toDo.components.js
@@ -0,0 +1,364 @@
+﻿Bridge.assembly("Bridge.ToDo", function ($asm, globals) {
+    "use strict";
+
+    Bridge.define("Bridge.ToDo.Components.ButtonCheckAll", {
+        clicked: null,
+        _checkAll: false,
+        _thisButton: null,
+        ctor: function (selector) {
+            this.$initialize();
+            this._thisButton = $(selector);
+            this._thisButton.click(Bridge.fn.bind(this, $_.Bridge.ToDo.Components.ButtonCheckAll.f1));
+        },
+        setCheckAll: function (value) {
+            this._checkAll = value;
+            this.changeIcon();
+        },
+        changeIcon: function () {
+            if (this._checkAll) {
+                //change icon
+                this._thisButton.removeClass("glyphicon-chevron-down");
+                this._thisButton.addClass("glyphicon-ok");
+            } else {
+                this._thisButton.removeClass("glyphicon-ok");
+                this._thisButton.addClass("glyphicon-chevron-down");
+            }
+        }
+    });
+
+    var $_ = {};
+
+    Bridge.ns("Bridge.ToDo.Components.ButtonCheckAll", $_);
+
+    Bridge.apply($_.Bridge.ToDo.Components.ButtonCheckAll, {
+        f1: function () {
+            this._checkAll = !this._checkAll;
+            this.changeIcon();
+            !Bridge.staticEquals(this.clicked, null) ? this.clicked(this._checkAll) : null;
+        }
+    });
+
+    Bridge.define("Bridge.ToDo.Components.ButtonClearCompleted", {
+        onClick: null,
+        btnClearCompleted: null,
+        ctor: function (selector) {
+            this.$initialize();
+            this.btnClearCompleted = Bridge.cast(document.getElementById(selector), HTMLInputElement);
+            this.btnClearCompleted.onclick = Bridge.fn.combine(this.btnClearCompleted.onclick, Bridge.fn.bind(this, $_.Bridge.ToDo.Components.ButtonClearCompleted.f1));
+        },
+        show: function () {
+            this.btnClearCompleted.classList.remove("hidden");
+        },
+        hide: function () {
+            this.btnClearCompleted.classList.add("hidden");
+        }
+    });
+
+    Bridge.ns("Bridge.ToDo.Components.ButtonClearCompleted", $_);
+
+    Bridge.apply($_.Bridge.ToDo.Components.ButtonClearCompleted, {
+        f1: function () {
+            !Bridge.staticEquals(this.onClick, null) ? this.onClick() : null;
+        }
+    });
+
+    Bridge.define("Bridge.ToDo.Components.Footer", {
+        _footer: null,
+        ctor: function (selector) {
+            this.$initialize();
+            this._footer = Bridge.cast(document.getElementById("footer"), HTMLDivElement);
+        },
+        show: function () {
+            this._footer.removeAttribute("hidden");
+        },
+        hide: function () {
+            this._footer.setAttribute("hidden", "hidden");
+        }
+    });
+
+    Bridge.define("Bridge.ToDo.Components.InputToDoItem", {
+        onKeyDownEnter: null,
+        _thisInput: null,
+        ctor: function (selector) {
+            this.$initialize();
+            this._thisInput = $(selector);
+
+            this._thisInput.keyup(Bridge.fn.bind(this, $_.Bridge.ToDo.Components.InputToDoItem.f1));
+        },
+        getVal: function () {
+            return this._thisInput.val();
+        },
+        setVal: function (value) {
+            this._thisInput.val(value);
+        }
+    });
+
+    Bridge.ns("Bridge.ToDo.Components.InputToDoItem", $_);
+
+    Bridge.apply($_.Bridge.ToDo.Components.InputToDoItem, {
+        f1: function (key) {
+            if (key.keyCode === 13) {
+                !Bridge.staticEquals(this.onKeyDownEnter, null) ? this.onKeyDownEnter() : null;
+            }
+        }
+    });
+
+    Bridge.define("Bridge.ToDo.Components.RadioButtonQueryItems", {
+        rbShowAll: null,
+        rbShowActive: null,
+        rbShowCompleted: null,
+        click: null,
+        selected: 0,
+        ctor: function (selectorShowAll, selectorShowActive, selectorShowCompleted) {
+            this.$initialize();
+            this.selected = Bridge.ToDo.Components.RadioButtonQueryItems.ShowOptions.All;
+            this.rbShowAll = Bridge.cast(document.getElementById(selectorShowAll), HTMLLabelElement);
+            this.rbShowActive = Bridge.cast(document.getElementById(selectorShowActive), HTMLLabelElement);
+            this.rbShowCompleted = Bridge.cast(document.getElementById(selectorShowCompleted), HTMLLabelElement);
+
+            this.rbShowAll.onclick = Bridge.fn.combine(this.rbShowAll.onclick, Bridge.fn.bind(this, $_.Bridge.ToDo.Components.RadioButtonQueryItems.f1));
+
+            this.rbShowActive.onclick = Bridge.fn.combine(this.rbShowActive.onclick, Bridge.fn.bind(this, $_.Bridge.ToDo.Components.RadioButtonQueryItems.f2));
+
+            this.rbShowCompleted.onclick = Bridge.fn.combine(this.rbShowCompleted.onclick, Bridge.fn.bind(this, $_.Bridge.ToDo.Components.RadioButtonQueryItems.f3));
+
+        }
+    });
+
+    Bridge.ns("Bridge.ToDo.Components.RadioButtonQueryItems", $_);
+
+    Bridge.apply($_.Bridge.ToDo.Components.RadioButtonQueryItems, {
+        f1: function () {
+            this.selected = Bridge.ToDo.Components.RadioButtonQueryItems.ShowOptions.All;
+            !Bridge.staticEquals(this.click, null) ? this.click(this.selected) : null;
+        },
+        f2: function () {
+            this.selected = Bridge.ToDo.Components.RadioButtonQueryItems.ShowOptions.Active;
+            !Bridge.staticEquals(this.click, null) ? this.click(this.selected) : null;
+        },
+        f3: function () {
+            this.selected = Bridge.ToDo.Components.RadioButtonQueryItems.ShowOptions.Completed;
+            !Bridge.staticEquals(this.click, null) ? this.click(this.selected) : null;
+        }
+    });
+
+    Bridge.define("Bridge.ToDo.Components.RadioButtonQueryItems.ShowOptions", {
+        $kind: "enum",
+        statics: {
+            All: 0,
+            Active: 1,
+            Completed: 2
+        }
+    });
+
+    Bridge.define("Bridge.ToDo.Components.ToDoItem", {
+        deleteClicked: null,
+        completeClicked: null,
+        _complete: false,
+        _label: "",
+        _cbItem: null,
+        _pLabel: null,
+        _buttonDel: null,
+        _divMainItem: null,
+        config: {
+            init: function () {
+                this._id = new System.Guid();
+            }
+        },
+        ctor: function (label) {
+            this.$initialize();
+            this._id = System.Guid.newGuid();
+            this._label = label;
+            this.initComponents(this._label);
+        },
+        getLabel: function () {
+            return this._label;
+        },
+        getDivItem: function () {
+            return this._divMainItem;
+        },
+        getItemID: function () {
+            return this._id.toString();
+        },
+        getComplete: function () {
+            return this._complete;
+        },
+        setComplete: function (value) {
+            this._complete = value;
+            this._cbItem.checked = this._complete;
+        },
+        initComponents: function (label) {
+            this.initCheckboxItem();
+            this.initLabel(label);
+            this.initButtonDel();
+            this._divMainItem = this.createHTMLItem();
+        },
+        initCheckboxItem: function () {
+            this._cbItem = Bridge.merge(document.createElement('input'), {
+                type: "checkbox"
+            } );
+            this._cbItem.onclick = Bridge.fn.combine(this._cbItem.onclick, Bridge.fn.bind(this, $_.Bridge.ToDo.Components.ToDoItem.f1));
+        },
+        initLabel: function (label) {
+            this._pLabel = Bridge.merge(document.createElement('p'), {
+                className: "text-left",
+                innerHTML: label
+            } );
+        },
+        initButtonDel: function () {
+            this._buttonDel = Bridge.merge(document.createElement('span'), {
+                className: "glyphicon glyphicon-remove-sign text-right"
+            } );
+            this._buttonDel.onclick = Bridge.fn.combine(this._buttonDel.onclick, Bridge.fn.bind(this, $_.Bridge.ToDo.Components.ToDoItem.f2));
+        },
+        createHTMLItem: function () {
+            var divRow = document.createElement('div');
+            divRow.className = "row";
+            divRow.id = this._id.toString();
+
+            var divCheckbox = Bridge.merge(document.createElement('div'), {
+                className: "col-md-1"
+            } );
+
+            divCheckbox.appendChild(this._cbItem);
+
+
+            var divLabel = Bridge.merge(document.createElement('div'), {
+                className: "col-md-10"
+            } );
+            divLabel.appendChild(this._pLabel);
+
+            var divButtonDel = Bridge.merge(document.createElement('div'), {
+                className: "col-md-1"
+            } );
+            divButtonDel.appendChild(this._buttonDel);
+
+
+            divRow.appendChild(divCheckbox);
+            divRow.appendChild(divLabel);
+            divRow.appendChild(divButtonDel);
+
+            return divRow;
+        }
+    });
+
+    Bridge.ns("Bridge.ToDo.Components.ToDoItem", $_);
+
+    Bridge.apply($_.Bridge.ToDo.Components.ToDoItem, {
+        f1: function () {
+            this._complete = !this._complete;
+            !Bridge.staticEquals(this.completeClicked, null) ? this.completeClicked(this._complete) : null;
+        },
+        f2: function () {
+            !Bridge.staticEquals(this.deleteClicked, null) ? this.deleteClicked() : null;
+        }
+    });
+
+    Bridge.define("Bridge.ToDo.Components.ToDoItemList", {
+        _divItemList: null,
+        _todoItemList: null,
+        ctor: function (selector) {
+            this.$initialize();
+            this._todoItemList = new (System.Collections.Generic.List$1(Bridge.ToDo.Components.ToDoItem))();
+            this._divItemList = Bridge.cast(document.getElementById(selector), HTMLDivElement);
+        },
+        add: function (todoItem) {
+            this._todoItemList.add(todoItem);
+            this._divItemList.appendChild(todoItem.getDivItem());
+        },
+        remove: function (todoItem) {
+            this._todoItemList.remove(todoItem);
+
+            if (this._divItemList.contains(todoItem.getDivItem())) {
+                this._divItemList.removeChild(todoItem.getDivItem());
+            }
+        },
+        showAll: function () {
+            var $t;
+            this.clearAllItem();
+            $t = Bridge.getEnumerator(this._todoItemList);
+            while ($t.moveNext()) {
+                var item = $t.getCurrent();
+                this._divItemList.appendChild(item.getDivItem());
+            }
+        },
+        showActive: function () {
+            var $t;
+            this.clearAllItem();
+            var active = System.Linq.Enumerable.from(this._todoItemList).where($_.Bridge.ToDo.Components.ToDoItemList.f1).toList(Bridge.ToDo.Components.ToDoItem);
+            $t = Bridge.getEnumerator(active);
+            while ($t.moveNext()) {
+                var item = $t.getCurrent();
+                this._divItemList.appendChild(item.getDivItem());
+            }
+        },
+        showCompleted: function () {
+            var $t;
+            this.clearAllItem();
+            var completed = System.Linq.Enumerable.from(this._todoItemList).where($_.Bridge.ToDo.Components.ToDoItemList.f2).toList(Bridge.ToDo.Components.ToDoItem);
+            $t = Bridge.getEnumerator(completed);
+            while ($t.moveNext()) {
+                var item = $t.getCurrent();
+                this._divItemList.appendChild(item.getDivItem());
+            }
+        },
+        /**
+         * Only clear UI item
+         *
+         * @instance
+         * @private
+         * @this Bridge.ToDo.Components.ToDoItemList
+         * @memberof Bridge.ToDo.Components.ToDoItemList
+         * @return  {void}
+         */
+        clearAllItem: function () {
+            while (this._divItemList.firstChild != null) {
+                this._divItemList.removeChild(this._divItemList.firstChild);
+            }
+        },
+        itemsLeft: function () {
+            var result = 0;
+            result = System.Linq.Enumerable.from(this._todoItemList).where($_.Bridge.ToDo.Components.ToDoItemList.f1).count();
+            return result;
+        },
+        completeCount: function () {
+            var result = 0;
+            result = System.Linq.Enumerable.from(this._todoItemList).where($_.Bridge.ToDo.Components.ToDoItemList.f2).count();
+            return result;
+        },
+        isEmpty: function () {
+            return System.Linq.Enumerable.from(this._todoItemList).count() === 0;
+        },
+        allComplete: function () {
+            return System.Linq.Enumerable.from(this._todoItemList).where($_.Bridge.ToDo.Components.ToDoItemList.f2).count() === System.Linq.Enumerable.from(this._todoItemList).count();
+        },
+        deleteCompleted: function () {
+            var $t;
+            var completed = System.Linq.Enumerable.from(this._todoItemList).where($_.Bridge.ToDo.Components.ToDoItemList.f2).toList(Bridge.ToDo.Components.ToDoItem);
+            $t = Bridge.getEnumerator(completed);
+            while ($t.moveNext()) {
+                var item = $t.getCurrent();
+                this.remove(item);
+            }
+        },
+        setAllComplete: function (complete) {
+            var $t;
+            $t = Bridge.getEnumerator(this._todoItemList);
+            while ($t.moveNext()) {
+                var item = $t.getCurrent();
+                item.setComplete(complete);
+            }
+        }
+    });
+
+    Bridge.ns("Bridge.ToDo.Components.ToDoItemList", $_);
+
+    Bridge.apply($_.Bridge.ToDo.Components.ToDoItemList, {
+        f1: function (e) {
+            return e.getComplete() === false;
+        },
+        f2: function (e) {
+            return e.getComplete() === true;
+        }
+    });
+});
---
+++ Bridge/output/bridge.toDo.js
@@ -0,0 +1,130 @@
+﻿Bridge.assembly("Bridge.ToDo", function ($asm, globals) {
+    "use strict";
+
+    Bridge.define("Bridge.ToDo.App", {
+        statics: {
+            buttonCheckAll: null,
+            inputToDoItem: null,
+            todoItemList: null,
+            footer: null,
+            itemLeft: null,
+            buttonClearCompleted: null,
+            radioButtonQueryItems: null,
+            initComponent: function () {
+                Bridge.ToDo.App.initButtonCheckAll();
+                Bridge.ToDo.App.initInputToDoItem();
+                Bridge.ToDo.App.initToDoItemList();
+                Bridge.ToDo.App.initFooter();
+                Bridge.ToDo.App.initItemLeft();
+                Bridge.ToDo.App.initButtonClearCompleted();
+                Bridge.ToDo.App.initRadioButtonQueryItems();
+            },
+            initRadioButtonQueryItems: function () {
+                Bridge.ToDo.App.radioButtonQueryItems = new Bridge.ToDo.Components.RadioButtonQueryItems("rbShowAll", "rbShowActive", "rbShowCompleted");
+                Bridge.ToDo.App.radioButtonQueryItems.click = Bridge.fn.combine(Bridge.ToDo.App.radioButtonQueryItems.click, $_.Bridge.ToDo.App.f1);
+            },
+            initButtonClearCompleted: function () {
+                Bridge.ToDo.App.buttonClearCompleted = new Bridge.ToDo.Components.ButtonClearCompleted("btnClearCompleted");
+                Bridge.ToDo.App.buttonClearCompleted.onClick = Bridge.fn.combine(Bridge.ToDo.App.buttonClearCompleted.onClick, $_.Bridge.ToDo.App.f2);
+            },
+            initFooter: function () {
+                Bridge.ToDo.App.footer = new Bridge.ToDo.Components.Footer("footer");
+            },
+            initItemLeft: function () {
+                Bridge.ToDo.App.itemLeft = Bridge.cast(document.getElementById("itemsLeft"), HTMLDivElement);
+            },
+            initButtonCheckAll: function () {
+                Bridge.ToDo.App.buttonCheckAll = new Bridge.ToDo.Components.ButtonCheckAll("#btnCheckAll");
+                Bridge.ToDo.App.buttonCheckAll.clicked = Bridge.fn.combine(Bridge.ToDo.App.buttonCheckAll.clicked, $_.Bridge.ToDo.App.f3);
+            },
+            initInputToDoItem: function () {
+                Bridge.ToDo.App.inputToDoItem = new Bridge.ToDo.Components.InputToDoItem("#inputToDoItem");
+                Bridge.ToDo.App.inputToDoItem.onKeyDownEnter = Bridge.fn.combine(Bridge.ToDo.App.inputToDoItem.onKeyDownEnter, $_.Bridge.ToDo.App.f4);
+            },
+            initToDoItemList: function () {
+                Bridge.ToDo.App.todoItemList = new Bridge.ToDo.Components.ToDoItemList("todoItemList");
+            },
+            addToDoItem: function (todoItemVal) {
+                if (System.String.isNullOrEmpty(todoItemVal)) {
+                    return;
+                }
+
+                var todoItem = new Bridge.ToDo.Components.ToDoItem(todoItemVal);
+                Bridge.ToDo.App.todoItemList.add(todoItem);
+
+                todoItem.deleteClicked = Bridge.fn.combine(todoItem.deleteClicked, function () {
+                    //Html5.Window.Alert("Delete Clicked");
+                    Bridge.ToDo.App.todoItemList.remove(todoItem);
+                    Bridge.ToDo.App.updateItemsLeft();
+                });
+
+                todoItem.completeClicked = Bridge.fn.combine(todoItem.completeClicked, $_.Bridge.ToDo.App.f5);
+
+                Bridge.ToDo.App.updateItemsLeft();
+                Bridge.ToDo.App.footer.show();
+            },
+            updateItemsLeft: function () {
+                var jmlItemsLeft = Bridge.ToDo.App.todoItemList.itemsLeft();
+                Bridge.ToDo.App.itemLeft.innerHTML = System.String.concat(jmlItemsLeft.toString(), " Items left");
+                Bridge.ToDo.App.setButtonClearCompletedVisible();
+                Bridge.ToDo.App.buttonCheckAll.setCheckAll(Bridge.ToDo.App.todoItemList.allComplete());
+
+                if (Bridge.ToDo.App.todoItemList.isEmpty()) {
+                    Bridge.ToDo.App.buttonCheckAll.setCheckAll(false);
+                    Bridge.ToDo.App.radioButtonQueryItems.selected = Bridge.ToDo.Components.RadioButtonQueryItems.ShowOptions.All;
+                    Bridge.ToDo.App.footer.hide();
+                }
+            },
+            setButtonClearCompletedVisible: function () {
+                var jumlahComplete = Bridge.ToDo.App.todoItemList.completeCount();
+                if (jumlahComplete > 0) {
+                    Bridge.ToDo.App.buttonClearCompleted.show();
+                } else {
+                    Bridge.ToDo.App.buttonClearCompleted.hide();
+                }
+            }
+        },
+        $main: function () {
+            Bridge.ToDo.App.initComponent();
+
+            // To confirm Bridge.NET is working:
+            // 1. Build this project (Ctrl + Shift + B)
+            // 2. Browse to file /Bridge/www/demo.html
+            // 3. Right-click on file and select "View in Browser" (Ctrl + Shift + W)
+            // 4. File should open in a browser, click the "Submit" button
+            // 5. Success!
+        }
+    });
+
+    var $_ = {};
+
+    Bridge.ns("Bridge.ToDo.App", $_);
+
+    Bridge.apply($_.Bridge.ToDo.App, {
+        f1: function () {
+            if (Bridge.ToDo.App.radioButtonQueryItems.selected === Bridge.ToDo.Components.RadioButtonQueryItems.ShowOptions.Active) {
+                Bridge.ToDo.App.todoItemList.showActive();
+            } else if (Bridge.ToDo.App.radioButtonQueryItems.selected === Bridge.ToDo.Components.RadioButtonQueryItems.ShowOptions.Completed) {
+                Bridge.ToDo.App.todoItemList.showCompleted();
+            } else { //show all
+                Bridge.ToDo.App.todoItemList.showAll();
+            }
+        },
+        f2: function () {
+            Bridge.ToDo.App.todoItemList.deleteCompleted();
+            Bridge.ToDo.App.updateItemsLeft();
+        },
+        f3: function (checkAll) {
+            Bridge.ToDo.App.todoItemList.setAllComplete(checkAll);
+            Bridge.ToDo.App.updateItemsLeft();
+        },
+        f4: function () {
+            //Html5.Window.Alert("OK Entered");
+            Bridge.ToDo.App.addToDoItem(Bridge.ToDo.App.inputToDoItem.getVal());
+            Bridge.ToDo.App.inputToDoItem.setVal("");
+        },
+        f5: function () {
+            Bridge.ToDo.App.updateItemsLeft();
+        }
+    });
+});
